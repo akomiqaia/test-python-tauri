787a1460b5ffa2369efccd2ec3717e234396918b
Akaki Mikaia <akakimikaia@gmail.com>
2024-10-22 17:04:15 +0100

    chore: simple pdf-extract with rust"

--- rust-pdf-extract-chunking/src/main.rs
+++ rust-pdf-extract-chunking/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+  let bytes = std::fs::read("docs/pdf2.0.pdf").unwrap();
+  let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
+
+  // write output to txt file
+  std::fs::write("output.txt", out.as_bytes()).unwrap();
+  println!("{}", out);
 }
